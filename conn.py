c1ae8f51845d43057c11e1ab6f27a84755ccfa06
Isaias Carvalho <isaias.carvalho@vortexz.com.br>
2024-11-18 14:20:15 -0400

    feat: add env config

--- conn.py
+++ conn.py
@@ -1,9 +1,25 @@
-import psycopg2.extras
+import os
+from dotenv import load_dotenv
+import psycopg2
 
-db = psycopg2.connect(
-    " dbname=pass"
-    " host=localhost"
-    " port=5434"
-    " password=$#77ALLI77#$"
-    " user=postgres"
-)
+
+env_file = ".env.dev"
+load_dotenv(env_file)
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+db_database = os.getenv("DB_DATABASE")
+
+try:
+    db = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        user=db_username,
+        password=db_password,
+        dbname=db_database,
+    )
+    print("Conexão com o banco de dados bem-sucedida!")
+except Exception as e:
+    print("Erro ao conectar ao banco de dados:", e)
